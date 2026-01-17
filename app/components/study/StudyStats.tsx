e5188dd8b84ab7a90dff1880b38d80ae1f690822
LucDub-code <contact@lucasdubeau.dev>
2026-01-17 02:44:44 +0100

    feat(study-ui): structure study page layout and centralize stroke/shadow design utilities

---
+++ app/components/study/StudyStats.tsx
@@ -0,0 +1,6 @@
+export default function StudyStats() {
+  return (
+    <div className="bg-white w-98 h-158 strokes-outer-md rounded-2xl">
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import StudyMain from "./components/study/StudyMain";
+import StudyStats from "./components/study/StudyStats";
 
 export default function StudyPage() {
   return (
-    <div></div>
+    <div className="flex gap-8 pt-8">
+      <StudyMain />
+      <StudyStats />
+    </div>
   );
 }
