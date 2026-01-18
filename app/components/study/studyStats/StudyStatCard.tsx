f59524edc0d238bd6977dab20a960ac5b1c5505e
LucDub-code <contact@lucasdubeau.dev>
2026-01-18 04:33:21 +0100

    feat(study): add StudyStats with reusable StatCard component

--- app/components/study/studyStats/StudyStatCard.tsx
+++ app/components/study/studyStats/StudyStatCard.tsx
@@ -1,5 +1,20 @@
-export default function StudyStatCard() {
+interface StudyStatCardProps {
+  title: string
+  value: number
+  color: string
+  icon: string
+}
+
+export default function StudyStatCard({ title, value, color, icon }: StudyStatCardProps) {
   return (
-    <div>StudyStatCard</div>
+    <article className="flex rounded-xl w-86 h-30 strokes-sm overflow-hidden">
+      <div className="w-[70%] flex flex-col m-5">
+        <p className="text-preset-4-medium">{title}</p>
+        <p className="mt-auto text-preset-1">{value}</p>
+      </div>
+      <div className={`w-[30%] border-l border-ink ${color} flex items-center justify-center`}>
+        <img src={icon} alt="" className="w-6" />
+      </div>
+    </article>
   )
 }
--- app/components/study/studyStats/StudyStats.tsx
+++ app/components/study/studyStats/StudyStats.tsx
@@ -1,6 +1,21 @@
+import StudyStatCard from "./StudyStatCard"
+
+const statsData = [
+    { title: "Total des cartes", value: 0, color: "bg-light-blue", icon: "/icons/icon-stats-total.svg" },
+    { title: "Mémorisées", value: 0, color: "bg-teal", icon: "/icons/icon-stats-mastered.svg" },
+    { title: "En cours", value: 0, color: "bg-salmon", icon: "/icons/icon-stats-in-progress.svg" },
+    { title: "Non commencées", value: 0, color: "bg-pink", icon: "/icons/icon-stats-not-started.svg" },
+  ]
+
 export default function StudyStats() {
   return (
-    <div className="bg-white w-98 h-158 strokes-outer-md rounded-2xl">
-    </div>
+    <section className="flex flex-col p-6 bg-white w-98 h-158 strokes-md rounded-2xl">
+      <h2 className="mb-3 text-preset-2">Statistiques d’étude</h2>
+      <div className="flex flex-col gap-5">
+        {statsData.map((stat) => (
+            <StudyStatCard key={stat.title} {...stat} />
+          ))}
+      </div>
+    </section>
   )
 }
