cd143643495887df98bcbc143037eaf290523b0f
LucDub-code <contact@lucasdubeau.dev>
2026-01-20 23:52:00 +0100

    feat(filters): complete category filter implementation for both pages

--- app/components/cards/CardGrid.tsx
+++ app/components/cards/CardGrid.tsx
@@ -1,6 +1,15 @@
+"use client"
+
+import { useCardFilterStore } from "@/app/stores/cardFilterStore"
+
 import Card from "./Card"
 
 export default function CardGrid() {
+
+  const { selectedCategories } = useCardFilterStore()
+
+  console.log(selectedCategories)
+
   return (
     <div className="grid grid-cols-3 gap-6">
       <Card />
--- app/components/cards/CardMenu.tsx
+++ app/components/cards/CardMenu.tsx
@@ -1,27 +1,17 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react" 
+import { useState, useRef } from "react"
+import { useClickOutside } from "@/app/hooks/useClickOutside"
 
 export default function CardMenu() {
 
   const [isOpen, setIsOpen] = useState(false)
 
   const menuRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {                                                                                  
-      function handleClickOutside(event: MouseEvent) {                                                 
-        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {                      
-          setIsOpen(false)                                                                             
-        }                                                                                              
-      } if (isOpen) {                                                                                    
-        document.addEventListener("click", handleClickOutside)                                         
-      } return () => {                                                                                   
-        document.removeEventListener("click", handleClickOutside)                                      
-      }                                                                                                
-    }, [isOpen])
+  useClickOutside(menuRef, () => setIsOpen(false), isOpen)
 
   return (
-    <div ref={menuRef} className="relative flex items-center h-full pl-4 border-l border-ink shadow-menu">
+    <div ref={menuRef} className="relative flex items-center h-full pl-4 border-l border-ink">
       <button 
         className={`cursor-pointer border rounded-sm ${isOpen ? "border-ink shadow-[2px_2px_0_var(--ink)]" : "border-transparent hover:border-ink hover:rounded-sm hover:shadow-[2px_2px_0_var(--ink)]"}`} 
         aria-label="Menu de la carte" 
@@ -32,7 +22,7 @@ export default function CardMenu() {
         <img src="/icons/icon-menu.svg" alt="" className="w-6" />
       </button>
       {isOpen && (
-        <menu role="menu" className="absolute bottom-15 -right-3.5 border rounded-lg border-ink list-none">
+        <menu role="menu" className="absolute bottom-15 -right-3.5 border rounded-lg border-ink list-none shadow-menu">
           <li role="menuitem">
             <button className="flex items-center w-full gap-1 py-2 pl-4 pr-8 bg-white border-b rounded-t-lg cursor-pointer text-preset-5 hover:bg-background border-ink">
               <img src="/icons/icon-edit.svg" alt="" className="w-4" />
